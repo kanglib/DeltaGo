3fa6f8d7cbc5828bebc6dc9f77423d556d8a10fc
Kang Hyeonwoo <kanglib14@gmail.com>
2017-03-03 09:31:05 +0900

    3.0 update

--- deltago.py
+++ deltago.py
@@ -1,15 +1,14 @@
 # HexChat w/ Python 3
-"""GoN regex challenge...?"""
 
 import hexchat
 import random
 import re
 
 __module_name__ = "DeltaGo"
-__module_version__ = "2.5"
+__module_version__ = "3.0"
 __module_description__ = "The Seed of Slackbot Go, a GoN AI developed by ShallowMind Corporation"
 
-whitelist = ["#nb", "#test_for", "#yb"]
+whitelist = ["#test_for", "#yb"]
 
 def bot_write(channel, message, su=0):
     if su == 0:
@@ -38,17 +37,11 @@ def message_cb(word, word_eol, userdata):
         return
 
     bot_write_ex(c, s, ":gaon:", "아이도루")
-    bot_write_ex(c, s, "~이미늦었다~", "트윗덱")
     bot_write_ex(c, s, "That bad love", "[Cc]olmar")
-    bot_write_ex(c, s, "twitter.com", "해로운 새")
     bot_write_ex(c, s, "그 못된 사랑", "꼴마르")
     bot_write_ex(c, s, "그녀를 java요..", "var로 char서")
     bot_write_ex(c, s, "그녀를 java요..", "발로 char서")
     bot_write_ex(c, s, "그녀를 java요..", "발로 차서")
-    bot_write_ex(c, s, "숨-죽이기", "인성[왕킹]")
-    bot_write_ex(c, s, "안녕!", r"안녕[!\?]?\b")
-    bot_write_ex(c, s, "좆무위키 꺼라", "좆무위키")
-    bot_write_ex(c, s, "ㅎㅎㅎ", r"\bㅎㅎㅎ$")
     bot_write_ex(c, s, "해로운 새", "파랑새")
     bot_write_ex(c, s, random.choice(["~하지말아라~", "~해도된다~", "~선동과 날조~", "~선조와 날동~"]), "[트투][위이][터타]")
     bot_write_ex(c, s, random.choice(["~하지말아라~", "~해도된다~", "~선동과 날조~", "~선조와 날동~"]), "짹짹이")
